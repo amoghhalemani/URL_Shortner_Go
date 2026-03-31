ee85088d49b75623ac326c7593611b2a60f74a2b
Amoghhalemani <amoghhalemani@gmail.com>
2026-03-31 22:09:36 +0530

    declared the AppEnv struct

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+//This file is the main orchestrator of the application. It initializes the application environment and starts the server.
+
+import (
+	"database/sql"
+)
+
+// AppEnv holds the application environment, which includes the database connection.
+type AppEnv struct {
+	DB *sql.DB
+}
+
 func main() {
-	print("Hello, World!")
+
 }
